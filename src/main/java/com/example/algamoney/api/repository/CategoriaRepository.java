23ac8661fb757b264adae8dbf08559f7c955d4b4
GiovannaRiqueti <giovannariqueti@gmail.com>
2017-08-22 14:19:51 -0300

    Primeiro recurso criado

--- src/main/java/com/example/algamoney/api/repository/CategoriaRepository.java
+++ src/main/java/com/example/algamoney/api/repository/CategoriaRepository.java
@@ -2,6 +2,8 @@ package com.example.algamoney.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.algamoney.api.model.Categoria;
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 
 }
