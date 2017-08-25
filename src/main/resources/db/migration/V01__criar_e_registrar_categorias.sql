1719247fa4a9e80e31f656ce65d1db907c1c1546
GiovannaRiqueti <giovannariqueti@gmail.com>
2017-08-25 17:43:09 -0300

    Comecei lançamentos, ainda com erros

--- src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -3,6 +3,3 @@ CREATE TABLE categoria(
 	nome VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-
---
+++ src/main/resources/db/migration/V05__inserir_lancamento.sql
@@ -0,0 +1,6 @@
+INSERT INTO lancamentos (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) VALUES
+ ('Salario Mensal', '2017-06-06', null, 6500.0, 'Distribuição de livros', 'RECEITA', 1, 1);
+INSERT INTO lancamentos (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) VALUES
+('Bahamas', '2017-07-02', '2017-09-10', 100.32, null, null, 'DESPESA', 2, 2);
+INSERT INTO lancamentos (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) VALUES
+('Top Club', '2016-08-09', '2017-09-05', 120, null, 'Festa', 'RECEITA', 3, 3);
