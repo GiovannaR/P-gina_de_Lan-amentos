1c8b032fa140c54edfa9c34c456774dad72cac98
GiovannaRiqueti <giovannariqueti@gmail.com>
2017-08-29 14:11:51 -0300

    Classe Lançamento com Get e Post prontos

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -1,14 +1,17 @@
 package com.example.algamoney.api.resource;
 
+import com.example.algamoney.api.com.example.algamoney.api.event.RecursoEventCriado;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.service.LancamentoService;
 import com.example.algamoney.api.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.List;
 
@@ -19,15 +22,29 @@ public class LancamentoResource {
     @Autowired
     private LancamentoService lancamentoservice;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Lancamento> listar (){
         return lancamentoservice.listar();
     }
 
-    /*@GetMapping("{codigo}")
+    @GetMapping("{codigo}")
     public Lancamento buscarpeloCodigo (@PathVariable Long codigo){
+        return lancamentoservice.buscar(codigo);
+    }
 
-    }*/
+    @PostMapping
+    @ResponseStatus (HttpStatus.CREATED)
+    public ResponseEntity<Lancamento> salvar (@Valid @RequestBody Lancamento lancamento, HttpServletResponse response){
+        Lancamento lancamentosalvo = lancamentoservice.salvarLancamento(lancamento);
+
+        publisher.publishEvent(new RecursoEventCriado(this, response, lancamentosalvo.getCodigo()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentosalvo);
+
+    }
 
 
 
--- src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -4,6 +4,7 @@ package com.example.algamoney.api.service;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,14 +19,19 @@ public class LancamentoService {
         return lancamentoRepository.findAll();
     }
 
-    /*public Lancamento buscar (long id){
+    public Lancamento buscar (long id){
         Lancamento lancamento = lancamentoRepository.findOne(id);
 
         if (lancamento == null){
             new EmptyResultDataAccessException(1);
         }
         return lancamento;
-    }*/
+    }
+
+    public Lancamento salvarLancamento (Lancamento lancamento){
+        lancamento.setCodigo(null);
+        return lancamentoRepository.save(lancamento);
+    }
 
 
 
